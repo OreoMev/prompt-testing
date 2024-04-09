916c7fa0814ba9fc6766fa7b5a8fb616d2210e1f
OreoMev <122867899+OreoMev@users.noreply.github.com>
2024-04-09 13:53:34 +0200

    Add argument parsing and error handling

--- test.py
+++ test.py
@@ -1,30 +1,49 @@
 import requests
-seed=323156744 #You can specify seed or use any random number generator
+import argparse
+import random
+
 def read_file_into_array(file_name):
     try:
         with open(file_name, 'r') as file:
             lines = file.readlines()
-            # Remove trailing newline characters
             lines = [line.strip() for line in lines]
             return lines
     except FileNotFoundError:
-        print("File not found:", file_name)
+        raise Exception("File not found:", file_name)
         return []
-file_name = "prompts.txt" #Each prompt should be in a new line
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-s', '--seed') 
+parser.add_argument('-m', '--models') 
+parser.add_argument('-p', '--prompts')  
+args = parser.parse_args()
+if (args.seed != None):
+    seed=args.seed
+else:
+    seed=random.randint(1, 999999) #if no seed is specified, random seed is created
+    print("Random seed selected: "+ seed )
+    print ("To specify the seed run with -s seed argument, where seed is any positive integer")
+if (args.prompts != None):
+    file_name = args.prompts 
+else:
+    print ("No file for prompts was specified, defaulting to prompts.txt")
+    file_name= "prompts.txt"
 prompts = read_file_into_array(file_name)
-models=[
-    "mistral",
-    "gemma:2b"
-]
+
+if (args.models != None):
+    models=args.models.split(",")
+else:
+    raise Exception("Specify the models with -m model1,model2,model3 and make sure you have this models installed in local Ollama instalations")
 
 for m in models:
     for p in prompts:
         body={
-        "model": m,
+        "model": m.strip(),
         "prompt": p,
         "stream": False,
         "options": {
-            "seed": seed,
+            "seed": int(seed),
             "temperature": 0
         }
         }
